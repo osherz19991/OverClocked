a826d0b9056832108c3b25c9af74ed9d0e3be7e3
AlonKigelman <72994068+AlonKigelman@users.noreply.github.com>
2024-04-14 23:13:59 +0300

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './Components/Header'
 
 export const App = () => {
-  return <h1>Welcome to OverClocked</h1>
-  
+  return(
+    <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+      <h1>Welcome to OverClocked</h1>
+      </Container>
+</main>
+</>
+);
 };
 export default App;
