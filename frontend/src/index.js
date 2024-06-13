d07a26365d8d583ea6eb6bff7d659b0a5738aff7
osher <osherzafar1973@gmail.com>
2024-06-14 02:20:12 +0300

    s

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
-} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import HomeScreen from './Screens/HomeScreen';
@@ -24,39 +18,36 @@ import AboutUs from './Screens/AboutUsScreen';
 import ContactUs from './Screens/ContactUs';
 import ForumScreen from './Screens/ForumScreen';
 import AdminScreen from './Screens/AdminScreen';
-import reportWebVitals from './reportWebVitals';
 import ForumHistory from './Screens/ForumHistory';
 import PostScreen from './Screens/PostScreen';
+import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path ="/" element = {<App />}>
-    <Route index={true} path="/" element={<HomeScreen/>} />
-    <Route path="/product/:id" element={<ProductScreen/>} />
-    <Route path="/signup" element={<SignUpScreen/>} />
-    <Route path="/signin" element={<SigninScreen/>} />
-    <Route path="/userInfo" element={<UserScreen/>} />
-    <Route path="/cart" element={<CartScreen/>} />
-    <Route path="/checkout" element={<CheckoutScreen/>} />
-    <Route path="/orderHistory" element={<OrderHistoryScreen/>} />
-    <Route path="/productsScreen" element={<ProductsScreen/>} />
-    <Route path="/password-reset" element={<PasswordReset />} />
-    <Route path="/contact-us" element={<ContactUs />} />
-    <Route path="/about-us" element={<AboutUs/>} />
-    <Route path="/forum" element={<ForumScreen/>} />
-    <Route path="/admin" element={<AdminScreen/>} />
-    <Route path="/Post/:id" element={<PostScreen/>} />
-    <Route path="/ForumHistory" element={<ForumHistory/>} />
-    </Route>
-  )
-)
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-   <RouterProvider  router = {router} />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomeScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
+          <Route path="/signup" element={<SignUpScreen />} />
+          <Route path="/signin" element={<SigninScreen />} />
+          <Route path="/userInfo" element={<UserScreen />} />
+          <Route path="/cart" element={<CartScreen />} />
+          <Route path="/checkout" element={<CheckoutScreen />} />
+          <Route path="/orderHistory" element={<OrderHistoryScreen />} />
+          <Route path="/productsScreen" element={<ProductsScreen />} />
+          <Route path="/password-reset" element={<PasswordReset />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/forum" element={<ForumScreen />} />
+          <Route path="/admin" element={<AdminScreen />} />
+          <Route path="/Post/:id" element={<PostScreen />} />
+          <Route path="/ForumHistory" element={<ForumHistory />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
 
 reportWebVitals();
