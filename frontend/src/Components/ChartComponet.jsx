8b680ce98ccf7662dff7aebad7b37bbeba00fb28
osher <osherzafar1973@gmail.com>
2024-07-18 00:21:56 +0300

    a

--- frontend/src/Components/ChartComponet.jsx
+++ frontend/src/Components/ChartComponet.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Chart } from 'chart.js/auto';
+import Chart from 'chart.js/auto';
 
 const ChartComponent = ({ data, chartType }) => {
   const chartRef = useRef(null);
