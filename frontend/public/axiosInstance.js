d714a5f5476943ae992c0c4b78135d458f025d0c
osher <osherzafar1973@gmail.com>
2024-07-18 02:49:35 +0300

    l

--- frontend/public/axiosInstance.js
+++ frontend/public/axiosInstance.js
@@ -2,7 +2,6 @@
 
 const instance = axios.create({
   baseURL: "https://overclocked.onrender.com", // Use your backend URL from environment variable
-  timeout: 10000, // Optional timeout configuration
   headers: {
     'Content-Type': 'application/json', // Example headers if needed
   },
